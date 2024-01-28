cd4e3802ba3a6308fe57bd8aff0c220779e2cba2
Abdo Deeb <deebabdo51@gmail.com>
2024-01-29 00:21:49 +0200

    abdo

---
+++ js/JS.js
@@ -0,0 +1,16 @@
+
+function navigate() {
+    window.location.href = "order.html"
+}
+function navigateToHome() {
+    window.location.href = "index.html"
+}
+function viewalert() {
+    alert("Added into Cart,please go to delivery info")
+}
+function viewalert2() {
+    alert("Thanks for Your Message")
+}
+function viewalert3() {
+    alert("Your order will be delivered soon")
+}
